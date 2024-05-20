21a1a30c1b4422ae294fc0a1e98ab4a990e2e64c
Aleksandra Zinoveva <alisenz@icloud.com>
2024-05-20 15:38:14 +0200

    removed startup file

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying custom default.conf over to /etc/nginx/sites-available/default.conf"
-cp /home/site/wwwroot/default.conf /etc/nginx/sites-available/default
-service nginx reload
-npm run build
